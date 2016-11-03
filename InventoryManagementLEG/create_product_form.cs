a81ee0749b542c4f88d91531f6d2c709a831c473
Landon Gardner <leg007@uark.edu>
2016-11-03 13:28:53 -0500

    add create product screen

---
+++ InventoryManagementLEG/create_product_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementLEG
+{
+    public partial class create_product_form : Form
+    {
+        public create_product_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- InventoryManagementLEG/issue_goods_form.Designer.cs
+++ InventoryManagementLEG/issue_goods_form.Designer.cs
@@ -48,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(53, 367);
+            this.button1.Location = new System.Drawing.Point(21, 335);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
